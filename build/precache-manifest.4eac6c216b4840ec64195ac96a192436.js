3a6fb0398e3841fa99f9d484112d68630ffb5003
tudt <tudt.erp@gmail.com>
2020-03-28 00:06:25 +0700

    init

---
+++ build/precache-manifest.4eac6c216b4840ec64195ac96a192436.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80e48fd29e27bfccf0db71d4f9fe835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41a0e2a38004f39c5bc8",
+    "url": "/static/css/2.69f9549e.chunk.css"
+  },
+  {
+    "revision": "70156be61ccc1ee05ce1",
+    "url": "/static/css/main.fe366f48.chunk.css"
+  },
+  {
+    "revision": "41a0e2a38004f39c5bc8",
+    "url": "/static/js/2.41a0e2a3.chunk.js"
+  },
+  {
+    "revision": "70156be61ccc1ee05ce1",
+    "url": "/static/js/main.70156be6.chunk.js"
+  },
+  {
+    "revision": "b6b06c58c396f9398061",
+    "url": "/static/js/runtime~main.b6b06c58.js"
+  },
+  {
+    "revision": "537dd20aca5a2ef744fcca3701994153",
+    "url": "/static/media/404.537dd20a.png"
+  },
+  {
+    "revision": "cd3f84e9fabfbe22f8c63edef3e769e7",
+    "url": "/static/media/bg_404.cd3f84e9.png"
+  },
+  {
+    "revision": "fec6134b9773126fdc216a985067485f",
+    "url": "/static/media/circle.fec6134b.svg"
+  },
+  {
+    "revision": "6cd04c4e80726f49e43dd72e2a0ac2c8",
+    "url": "/static/media/logo_dark.6cd04c4e.svg"
+  },
+  {
+    "revision": "07f37aef367aabd77c0cc48bebb6789d",
+    "url": "/static/media/logo_light.07f37aef.PNG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
