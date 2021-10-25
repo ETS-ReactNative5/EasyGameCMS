b13c452f09e3bd19a8f8e519cd82ea54add7e3f4
TuDT <tudt.erp@gmail.com>
2021-10-25 13:57:51 +0700

    rename

--- src/containers/Dashboards/BossScore/index.jsx
+++ src/containers/Dashboards/BossScore/index.jsx
@@ -7,7 +7,6 @@ import AccountOutlineIcon from 'mdi-react/AccountOutlineIcon';
 import MagnifyIcon from 'mdi-react/MagnifyIcon';
 import CloseCircleOutlineIcon from 'mdi-react/CloseCircleOutlineIcon';
 
-
 import BossScoreView from './components/BossScoreView';
 
 //import PlaceOrder from './components/PlaceOrder';
@@ -39,40 +38,39 @@ class CryptoDashboard extends PureComponent {
     this.getBossScore();
   }
 
-  getBossScore = async() => {
+  getBossScore = async () => {
     let lsSurvival = [];
-    await  
-        axios
-        .post(config.cms_url + config.url_getBossScore, {
-          userID: sessionStorage.getItem('userID'),
-          BossScore: this.state.levelBoss,
-        })
-        .then(function(response) {
-          console.log(response);
-          if (response.status === 200) {
-            let data = response.data;
-            if (data.status === 'ok') {
-              lsSurvival = data.data;
-            }
+    await axios
+      .post(config.base_url + config.url_getBossScore, {
+        userID: sessionStorage.getItem('userID'),
+        BossScore: this.state.levelBoss,
+      })
+      .then(function(response) {
+        console.log(response);
+        if (response.status === 200) {
+          let data = response.data;
+          if (data.status === 'ok') {
+            lsSurvival = data.data;
           }
-        })
-        .catch(function(error) {
-          console.log(error);
-        })
-        .then(() => {
-          this.setState({
-            listSurvival: lsSurvival.map((item) => {
-              let user = {};
-              user.UserId = item.UserId;
-              user.DisplayName = item.PlayerData.DisplayName;
-              user.Country = item.PlayerData.Country;
-              user.Score = item.Score;
-              return user;
-            }),
-            //lsPackageIAP: lsPackage,
-          });
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+      .then(() => {
+        this.setState({
+          listSurvival: lsSurvival.map((item) => {
+            let user = {};
+            user.UserId = item.UserId;
+            user.DisplayName = item.PlayerData.DisplayName;
+            user.Country = item.PlayerData.Country;
+            user.Score = item.Score;
+            return user;
+          }),
+          //lsPackageIAP: lsPackage,
         });
-  }
+      });
+  };
   onDeleteCryptoTableData = (index, e) => {
     const { dispatch, cryptoTable } = this.props;
     e.preventDefault();
@@ -80,17 +78,17 @@ class CryptoDashboard extends PureComponent {
     arrayCopy.splice(index, 1);
     dispatch(deleteCryptoTableData(arrayCopy));
   };
-  
+
   OnSearchClick = (e) => {
     if (e) e.preventDefault();
     this.getBossScore();
-  }
+  };
 
   OnClearClick = (e) => {
     e.preventDefault();
 
     this.setState({
-     levelBoss: 0,
+      levelBoss: 0,
     });
   };
 
@@ -108,23 +106,23 @@ class CryptoDashboard extends PureComponent {
     }
   }
 
-  inCreaseLevelBoss = async() => {
-    this.setState({levelBoss: this.state.levelBoss +1 }, () => {
-    //  console.log(this.state.levelBoss)
-      this.getBossScore()
-    })
-  }
+  inCreaseLevelBoss = async () => {
+    this.setState({ levelBoss: this.state.levelBoss + 1 }, () => {
+      //  console.log(this.state.levelBoss)
+      this.getBossScore();
+    });
+  };
 
-  deCreaseLevelBoss = async() => {
-    if(this.state.levelBoss) {
-    this.setState({levelBoss: this.state.levelBoss -1 }, () => {
-     // console.log(this.state.levelBoss)
-      this.getBossScore()
-    })
-  }
-  }
+  deCreaseLevelBoss = async () => {
+    if (this.state.levelBoss) {
+      this.setState({ levelBoss: this.state.levelBoss - 1 }, () => {
+        // console.log(this.state.levelBoss)
+        this.getBossScore();
+      });
+    }
+  };
   render() {
-    const levelBoss = 'Level: ' + this.state.levelBoss
+    const levelBoss = 'Level: ' + this.state.levelBoss;
     // var chartStage;
     // if (this.state.lsWinrate.length > 1)
     //   chartStage = <BtcEth data={this.state.lsWinrate} theme={theme.className} callback={this.onStageCallback} />;
@@ -132,42 +130,39 @@ class CryptoDashboard extends PureComponent {
     return (
       <Container className='dashboard'>
         <Row>
-          <Col><h3 className='page-title'>Jackal Leaderboard</h3> </Col>
-          <Container >
-        <Col className='inline'>
-                      <div className='form__form-group-field'>
-                        <div>
-                          <AccountOutlineIcon />
-                        </div>
-                        <input
-                          type = 'text'
-                          name='level'
-                          value={this.state.levelBoss}
-                          onKeyPress={this.OnkeyPress.bind(this)}
-                          onChange={this.onChangeValue}
-                          placeholder='level'
-                        />
-                      </div>
-                    </Col>
-                </Container>
+          <Col>
+            <h3 className='page-title'>Jackal Leaderboard</h3>{' '}
+          </Col>
+          <Container>
+            <Col className='inline'>
+              <div className='form__form-group-field'>
+                <div>
+                  <AccountOutlineIcon />
+                </div>
+                <input
+                  type='text'
+                  name='level'
+                  value={this.state.levelBoss}
+                  onKeyPress={this.OnkeyPress.bind(this)}
+                  onChange={this.onChangeValue}
+                  placeholder='level'
+                />
+              </div>
+            </Col>
+          </Container>
           <Col md={10} x1={1}>
             <Container style={{ textAlign: 'center', display: 'inline_block' }}>
-                <Button className='inlineBtn' 
-                        onClick={() => this.deCreaseLevelBoss()}>
-                  <p> Previous</p>
-                </Button>
-                <h4 
-                    style={{ display: 'inline-block', fontWeight: 'bold' }}> 
-                    {levelBoss} 
-                </h4>
-                <Button className='inlineBtn' onClick={() => this.inCreaseLevelBoss()}>
-                  <p>
-                    {'    '}
-                    Next
-                  </p>
-                </Button>
-                
-              </Container>
+              <Button className='inlineBtn' onClick={() => this.deCreaseLevelBoss()}>
+                <p> Previous</p>
+              </Button>
+              <h4 style={{ display: 'inline-block', fontWeight: 'bold' }}>{levelBoss}</h4>
+              <Button className='inlineBtn' onClick={() => this.inCreaseLevelBoss()}>
+                <p>
+                  {'    '}
+                  Next
+                </p>
+              </Button>
+            </Container>
           </Col>
         </Row>
 
